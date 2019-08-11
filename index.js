ccdfef6ab4871ce0ef72cb03296516ec983d9a69
William Verhaeghe <ikbendewilliam@hotmail.com>
2019-08-11 13:38:24 +0200

    added backPropagrate (WIP, only first column is correct atm)

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ machineLearningLib.matrixFunctions.createMatrix = function (width, height, stand
 
 machineLearningLib.matrixFunctions.matrixMultiplication = function (M1, M2) {
     if (M1[0].length !== M2.length) {
-        console.error("Error matrices cannot be multiplicated, sizes are not compatible");
+        console.error("Error matrices cannot be multiplicated, sizes " + M1.length + "x" + M1[0].length + " and " + M2.length + "x" + M2[0].length + " are not compatible");
         return "Error matrices cannot be multiplicated";
     }
     M3 = machineLearningLib.matrixFunctions.createMatrix(M1.length, M2[0].length);
@@ -90,6 +90,7 @@ machineLearningLib.matrixFunctions.matrixAddition = function (M1, M2) {
 
 machineLearningLib.matrixFunctions.matrixMultiplyingElements = function (M1, M2) {
     if (M1.length !== M2.length || M1[0].length !== M2[0].length) {
+        debugger;
         console.error("Error matrices cannot be multiplied, sizes are not compatible");
         return "Error matrices cannot be multiplied";
     }
@@ -123,6 +124,34 @@ machineLearningLib.matrixFunctions.addColumn = function (M, standardValue) {
     return M;
 }
 
+machineLearningLib.matrixFunctions.addRow = function (M, standardValue) {
+    if (standardValue === undefined) {
+        standardValue = 0;
+    }
+    M = JSON.parse(JSON.stringify(M));
+    M.unshift([]);
+    for (let i = 0; i < M[1].length; i++) {
+        M[0].push(standardValue);
+    }
+    return M;
+}
+
+machineLearningLib.matrixFunctions.removeColumn = function (M) {
+    M = JSON.parse(JSON.stringify(M));
+    for (let i = 0; i < M.length; i++) {
+        M[i] = M[i].splice(1, M[i].length - 1);
+    }
+    return M;
+}
+
+machineLearningLib.matrixFunctions.removeRow = function (M) {
+    M2 = [];
+    for (let i = 1; i < M.length; i++) {
+        M2.push(M[i]);
+    }
+    return M2;
+}
+
 machineLearningLib.matrixFunctions.sumMatrix = function (M) {
     let total = 0;
     for (let i = 0; i < M.length; i++) {
@@ -148,35 +177,88 @@ machineLearningLib.neuralNetworkFunctions.forwardPropagate = function (X, theta)
 // X = last layer
 // y = Labels
 // K = number of output units
-machineLearningLib.neuralNetworkFunctions.costFunction = function (X, y, K, lambda) {
+machineLearningLib.neuralNetworkFunctions.costFunction = function (X, y, K, thetas, lambda) {
     if (lambda === undefined) {
         lambda = 0;
     }
     let J = 0;
     for (k = 0; k < K; k++) {
         let Mx = [machineLearningLib.matrixFunctions.getColumn(X, k)];
-        console.log("Mx", Mx);
         let My1 = machineLearningLib.matrixFunctions.executeOnAllElements([y], function (x) { return -1 * (x === (k + 1)); });
-        console.log("My1", My1);
         let My2 = machineLearningLib.matrixFunctions.executeOnAllElements([y], function (x) { return 1 - 1 * (x === (k + 1)); });
-        console.log("My2", My2);
         let Mlog1 = machineLearningLib.matrixFunctions.executeOnAllElements(Mx, function (x) { return x <= 0 ? 0 : Math.log(x); });
-        console.log("Mlog1", Mlog1);
         let Mlog2 = machineLearningLib.matrixFunctions.executeOnAllElements(Mx, function (x) { return (1 - x) <= 0 ? 0 : -Math.log(1 - x); });
-        console.log("Mlog2", Mlog2);
         let M1 = machineLearningLib.matrixFunctions.matrixMultiplyingElements(My1, Mlog1);
-        console.log("M1", M1);
         let M2 = machineLearningLib.matrixFunctions.matrixMultiplyingElements(My2, Mlog2);
-        console.log("M2", M2);
         let M = machineLearningLib.matrixFunctions.matrixAddition(M1, M2);
-        console.log("M", M);
         J += 1 / X.length * machineLearningLib.matrixFunctions.sumMatrix(M);
-        console.log("J", J);
     }
-    // J = J + lambda / (2 * m) * (sum(sum(Theta1(:, 2: end).^ 2)) + sum(sum(Theta2(:, 2: end).^ 2)));
+
+    for (l = 0; l < thetas.length; l++) {
+        let T = machineLearningLib.matrixFunctions.removeColumn(thetas[l]);
+        let T2 = machineLearningLib.matrixFunctions.executeOnAllElements(T, function (x) { return Math.pow(x, 2) });
+        J += lambda / (2 * X.length) * machineLearningLib.matrixFunctions.sumMatrix(T2);
+    }
     return J;
 }
 
+machineLearningLib.neuralNetworkFunctions.backPropagrate = function (X, y, K, thetas, lambda) {
+    if (lambda === undefined) {
+        lambda = 0;
+    }
+    theta_grad = [];
+    for (i = 0; i < X.length; i++) {
+        let a = [];
+        let z = [];
+        a.push([X[i]]);
+        for (l = 0; l < thetas.length; l++) {
+            a[l] = machineLearningLib.matrixFunctions.addRow(machineLearningLib.matrixFunctions.transpose(a[l]), 1);
+            z[l + 1] = machineLearningLib.matrixFunctions.matrixMultiplication(thetas[l], a[l]);
+            a[l + 1] = machineLearningLib.matrixFunctions.sigmoid([z[l + 1]]);
+        }
+
+        a[thetas.length] = machineLearningLib.matrixFunctions.transpose(a[thetas.length]);
+        temp_y = machineLearningLib.matrixFunctions.createMatrix(K, 1, 0)
+        temp_y[y[i] - 1][0] = -1;
+
+        let delta = [];
+        delta[thetas.length + 1] = machineLearningLib.matrixFunctions.matrixAddition(a[thetas.length], temp_y);
+        for (l = thetas.length; l >= 1; l--) {
+            // console.log(l);
+            let d = delta[l + 1];
+            if (l !== thetas.length) {
+                d = machineLearningLib.matrixFunctions.removeRow(d);
+            }
+            d = machineLearningLib.matrixFunctions.matrixMultiplication(d, machineLearningLib.matrixFunctions.transpose(a[l - 1]));
+            if (theta_grad[l] === undefined) {
+                theta_grad[l] = machineLearningLib.matrixFunctions.createMatrix(d.length, d[0].length);
+            }
+            theta_grad[l] = machineLearningLib.matrixFunctions.matrixAddition(theta_grad[l], d);
+            // console.log(theta_grad);
+
+            if (l === 1) {
+                continue; // No need to calculate delta_1 since this is the input layer and this has no error
+            }
+            delta[l] = machineLearningLib.matrixFunctions.transpose(thetas[l - 1]);
+            // console.log(delta);
+            delta[l] = machineLearningLib.matrixFunctions.matrixMultiplication(delta[l], delta[l + 1]);
+            // console.log(delta);
+            let zd = machineLearningLib.matrixFunctions.sigmoidGradient(machineLearningLib.matrixFunctions.addColumn([z[l - 1]], 1));
+            // console.log(zd);
+            delta[l] = machineLearningLib.matrixFunctions.matrixMultiplyingElements(delta[l], machineLearningLib.matrixFunctions.transpose(zd));
+            // console.log(delta);
+        }
+    }
+
+    for (l = thetas.length; l >= 1; l--) {
+        theta_grad[l] = machineLearningLib.matrixFunctions.executeOnAllElements(theta_grad[l], function (x) { return x / X.length; });
+        let tg = machineLearningLib.matrixFunctions.removeColumn(thetas[l - 1]);
+        tg = machineLearningLib.matrixFunctions.executeOnAllElements(tg, function (x) { return x * lambda / X.length; });
+        theta_grad[l] = machineLearningLib.matrixFunctions.matrixAddition(theta_grad[l], machineLearningLib.matrixFunctions.addColumn(tg, 0));
+        // theta_grad[l] += [zeros(size(Theta1, 1), 1) (lambda / m .* Theta1(:,2:end))];
+    }
+    return theta_grad;
+}
 
 // Private functions
 
--- test/app.js
+++ test/app.js
@@ -36,10 +36,48 @@ theta2 = [[0.5403, -0.9111, -0.2752, 0.9912, -0.0133], [-0.9900, 0.0044, 0.9887,
 
 
 a2 = nn.forwardPropagate(mf.addColumn(X, 1), theta1);
-z2 = mf.matrixMultiplication(mf.addColumn(X, 1), mf.transpose(theta1));
-a2 = mf.sigmoid(z2);
 a3 = nn.forwardPropagate(mf.addColumn(a2, 1), theta2);
 
-J = nn.costFunction(a3, y, 4, 0);
-console.log(J);
-document.querySelector("#result").innerHTML = J;
+J = nn.costFunction(a3, y, 4, [theta1, theta2], 1);
+theta_grads = nn.backPropagrate(X, y, 4, [theta1, theta2], 1);
+document.querySelector("#result").innerHTML = JSON.stringify(theta_grads);
+console.log(theta_grads);
+
+/*
+for t = 1:m
+    at1 = X(t,:)';
+    at1 = [1; at1];
+    zt2 = Theta1 * at1;
+    at2 = [1;sigmoid(zt2)];
+    zt3 = Theta2 * at2;
+    at3 = sigmoid(zt3);
+
+    temp_y = zeros(num_labels, 1);
+    temp_y(y(t)) = 1;
+
+    delta_3 = at3 - temp_y;
+    delta_2 = Theta2' * delta_3 .* sigmoidGradient([1;zt2]);
+    delta_2 = delta_2(2:end);
+    Theta1_grad = Theta1_grad + delta_2 * (at1');
+    Theta2_grad = Theta2_grad + delta_3 * (at2');
+end
+
+Theta1_grad = Theta1_grad ./ m + [zeros(size(Theta1, 1), 1) (lambda / m .* Theta1(:,2:end))];
+Theta2_grad = Theta2_grad ./ m + [zeros(size(Theta2, 1), 1) (lambda / m .* Theta2(:,2:end))];
+
+
+Theta1_grad =
+
+   -0.0121   -0.0031    0.0038
+   -0.1278   -0.0042    0.0049
+    0.0426    0.0040   -0.0047
+    0.0997    0.0013   -0.0017
+
+    Theta2_grad =
+
+    0.2550    0.1826    0.1317    0.0693    0.1732
+   -0.0080   -0.0081   -0.0024   -0.0011   -0.0080
+    0.4669    0.3262    0.2487    0.1296    0.3077
+    0.3410    0.2397    0.1806    0.0939    0.2262
+
+*/
